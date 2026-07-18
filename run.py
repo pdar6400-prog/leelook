13edfcd069e713989cff55e3bc6aecd73aaf14cb
pdar6400-prog <pdar6400@gmail.com>
2026-07-18 21:10:24 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import k
+import le
 
 # Device Lock စနစ်ကို အရင်စစ်ဆေးပါမယ်
-if k.check_activation():
+if le.check_activation():
     # Activation အောင်မြင်ရင် Main Menu ကို ခေါ်ပါမယ်
-    k.main_menu()
+    le.main_menu()
   
